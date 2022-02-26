4bf956095e2c935c4124535e5272b1334ef26fa0
23rdPro <mailolumide@gmail.com>
2022-02-26 18:31:07 +0100

    changed allowed host

--- settings/prod.py
+++ settings/prod.py
@@ -4,4 +4,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['owiwi.herokuapp.com', ]
+ALLOWED_HOSTS = ['.herokuapp.com', ]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
