ae421ce9ebd8d47fcca9930f08db65fbf67381bc
23rdPro <mailolumide@gmail.com>
2022-03-04 13:44:11 +0100

    started allauth, refactored prod settings

--- settings/prod.py
+++ settings/prod.py
@@ -7,3 +7,5 @@ DEBUG = os.environ.get('PROD_DEBUG')
 ALLOWED_HOSTS = ['.herokuapp.com', ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
